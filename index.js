8e5923082ba930e4d1c44633b59632c1ef9a3177
Franco <francoramiresdosanto@gmail.com>
2022-03-17 00:41:50 -0300

    Capturando dados utilizando regex

--- index.js
+++ index.js
@@ -1,30 +1,6 @@
 const fs = require ("fs"); 
 const { encode } = require("punycode");
 
-function trataErro (erro) {
-    throw new Error (erro); 
-}
-
-// then || catch
-// function pegaArquivo(caminhoDoArquivo) {
-//     const encoding = "utf-8";
-
-//     fs.promises.readFile(caminhoDoArquivo, encoding)
-//     .then((texto) => console.log(texto))
-//     .catch((erro) => trataErro(erro))
-// }
-
-// async || await
-async function pegaArquivo(caminhoDoArquivo) {
-    const encoding = "utf-8"
-    try{
-        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
-        console.log(texto)
-    } catch(erro) {
-        trataErro(erro)
-    }
-}
-
 // function pegaArquivo (caminhoDoArquivo) {
 //     const encoding = "utf-8"
 
@@ -38,4 +14,40 @@ async function pegaArquivo(caminhoDoArquivo) {
 //     })
 // }
 
-pegaArquivo("texto1.md")
+// then || catch
+// function pegaArquivo(caminhoDoArquivo) {
+//     const encoding = "utf-8";
+
+//     fs.promises.readFile(caminhoDoArquivo, encoding)
+//     .then((texto) => console.log(texto))
+//     .catch((erro) => trataErro(erro))
+// }
+
+const texto = "São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.)"
+
+function extraiLinks(texto){
+    const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s]*.[^\s])*\)/gm; 
+    const linksExtraidos = texto.match(regex)
+
+    console.log(linksExtraidos)
+}
+
+function trataErro (erro) {
+    throw new Error (erro); 
+}
+
+extraiLinks(texto);
+
+// async || await
+async function pegaArquivo(caminhoDoArquivo) {
+    const encoding = "utf-8"
+    try{
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+        console.log(texto)
+    } catch(erro) {
+        trataErro(erro)
+    }
+}
+
+
+// pegaArquivo("texto1.md")
