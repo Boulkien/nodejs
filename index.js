eb46b6643a4f195523632eb3618b8ab3f731fc15
Franco <francoramiresdosanto@gmail.com>
2022-03-17 15:23:26 -0300

    Capturando dados de uma array com regex  através de funções Async

--- index.js
+++ index.js
@@ -23,31 +23,33 @@ const { encode } = require("punycode");
 //     .catch((erro) => trataErro(erro))
 // }
 
-const texto = "São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.)"
 
 function extraiLinks(texto){
-    const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s]*.[^\s])*\)/gm; 
-    const linksExtraidos = texto.match(regex)
+    const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm; 
+    const arrayResultados = [];
+    let temp;
 
-    console.log(linksExtraidos)
+    while ((temp = regex.exec(texto)) !== null) {
+        arrayResultados.push({ [temp[1]]: temp[2] })
+    }
+
+    return arrayResultados
 }
 
 function trataErro (erro) {
     throw new Error (erro); 
 }
 
-extraiLinks(texto);
-
 // async || await
 async function pegaArquivo(caminhoDoArquivo) {
     const encoding = "utf-8"
     try{
         const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
-        console.log(texto)
+        console.log(extraiLinks(texto))
     } catch(erro) {
         trataErro(erro)
     }
 }
 
 
-// pegaArquivo("texto1.md")
+pegaArquivo("texto1.md")
