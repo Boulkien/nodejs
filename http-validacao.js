cf1eb290f136c873d8b9bd4e503656c053661d4d
Franco <francoramiresdosanto@gmail.com>
2022-03-20 23:54:03 -0300

    Juntando Array de Links com Status Code e retornando resultado ao terminal

--- http-validacao.js
+++ http-validacao.js
@@ -1,25 +1,43 @@
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
+function manejaErros(erro) {
+    throw new Error(erro.message); 
+}
+
 async function checaStatus (arrayURLs) {
     //promises async || await
-    const arrayStatus = await Promise.all(arrayURLs.map(async url => {
-        const res = await fetch(url)
-        return res.status;
-    }))
-    return arrayStatus
+    try{
+        const arrayStatus = await Promise
+            .all(arrayURLs
+                .map(async url => {
+                    const res = await fetch(url)
+                    return `${res.status} - ${res.statusText}`;
+        }))
+        return arrayStatus
+    } catch(erro) {
+        manejaErros(erro)
+    }
 }
 
 function geraArrayDeURLs (arrayLinks){
     //loop for each key; value
     // object --> return values in a array
     // Object.values(objeto)
-    return arrayLinks.map(objetoLink => Object.values(objetoLink).join())
+    return arrayLinks
+        .map(objetoLink => Object
+                .values(objetoLink).join())
 }
 
 async function validaURLs(arrayLinks) {
     const links = geraArrayDeURLs(arrayLinks)
     const statusLinks = await checaStatus(links)
-    return statusLinks;
+    //spread operator
+    const resultados = arrayLinks.map((objeto, indice) => ({
+        ...objeto, 
+        status: statusLinks[indice] 
+    }))
+
+    return resultados; 
 }
 
 module.exports = validaURLs; 
