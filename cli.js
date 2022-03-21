c5f0de1258280590d6528bf493088a32c0ffe682
Franco <francoramiresdosanto@gmail.com>
2022-03-20 23:33:22 -0300

    Criando função para checar e lançar status de URL no terminal

--- cli.js
+++ cli.js
@@ -4,9 +4,9 @@ const validaURLs = require("./http-validacao")
 
 async function processaTexto (caminhoDeArquivo) {
     const resultado = await pegaArquivo(caminhoDeArquivo[2])
-    
+
     if (caminho [3] === "validar") {
-        console.log("Links Validados: ", validaURLs(resultado)); 
+        console.log("Links Validados: ", await validaURLs(resultado)); 
     } else {
         console.log("Lista de links: ", resultado)
     }
--- http-validacao.js
+++ http-validacao.js
@@ -1,5 +1,14 @@
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
+async function checaStatus (arrayURLs) {
+    //promises async || await
+    const arrayStatus = await Promise.all(arrayURLs.map(async url => {
+        const res = await fetch(url)
+        return res.status;
+    }))
+    return arrayStatus
+}
+
 function geraArrayDeURLs (arrayLinks){
     //loop for each key; value
     // object --> return values in a array
@@ -7,8 +16,10 @@ function geraArrayDeURLs (arrayLinks){
     return arrayLinks.map(objetoLink => Object.values(objetoLink).join())
 }
 
-function validaURLs(arrayLinks) {
-    return geraArrayDeURLs (arrayLinks)
+async function validaURLs(arrayLinks) {
+    const links = geraArrayDeURLs(arrayLinks)
+    const statusLinks = await checaStatus(links)
+    return statusLinks;
 }
 
 module.exports = validaURLs; 
